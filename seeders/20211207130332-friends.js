a2f92de24a3de02d293a5631fa4ed710e8525e8a
AdrianRotarasu <adrian.rotarasu@s.unibuc.ro>
2021-12-08 14:12:41 +0200

    .

--- seeders/20211207130332-friends.js
+++ seeders/20211207130332-friends.js
@@ -5,8 +5,9 @@ const db = require('../models');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const allUsers = await db.Users.findAll();
+    const n= await db.Users.findAll(friendsNumber);
     const coments = [];
-    for(let i = 0; i < db.Users.friendsNumber; i++) {
+    for(let i = 0; i < n; i++) {
       const userId = Math.floor(Math.random() * (allUsers.length - 1));
       coments.push({
         id: i,
